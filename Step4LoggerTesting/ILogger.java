92bbeeeecba044a081fa9fa4727b9a35bb37092e
Jonathan Windell <windelljonathan04@gmail.com>
2025-04-26 10:50:56 +0200

    Commit

--- Step4LoggerTesting/ILogger.java
+++ Step4LoggerTesting/ILogger.java
@@ -1,5 +1,9 @@
 package Step4LoggerTesting;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public interface ILogger {
 
     void log(String message);
@@ -12,6 +16,24 @@ public interface ILogger {
         }
     }
 
-    
+    public class FileLogger implements ILogger {
 
+        private String filePath;
+
+        public FileLogger(String filePath) {
+            this.filePath = filePath;
+        }
+
+        @Override
+        public void log(String message) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+                writer.write(message);
+                writer.newLine();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+    }
 }
